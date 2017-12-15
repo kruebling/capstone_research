716d8e879b3dda63205c0cdf832ecdcab7da43ba
kruebling <keegan.ruebling@gmail.com>
2017-12-15 13:33:04 -0800

    add exception to model to handle unitialized constant error on csv import

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,2 +1,3 @@
 class Weather < ApplicationRecord
+  self.inheritance_column = :_type_disabled
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 require 'csv'
 
 csv_text = File.read('../weather.csv')
+puts 'starting...this could take awhile'
 csv = CSV.parse(csv_text, :headers => true)
 csv.each do |row|
   Weather.create!(row.to_hash)
 end
+puts 'success'
